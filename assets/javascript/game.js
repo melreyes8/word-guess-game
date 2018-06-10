457162f90ea4c1dc195381c04f3447b6ec600877
Goldilocks <37794497+melreyes8@users.noreply.github.com>
2018-06-10 01:35:13 -0700

    tried to incorporate movie trailer or movie poster to JS when User guessed the word correctly. Need assistance with this one.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
-// words to guess
-var movieTitles = ['salt', 'argo', 'thor', 'halloween', 'jumanji', 'cinderella'];
+// One-word movie titles to guess
+var movieTitles = ['salt', 'argo', 'thor', 'halloween', 'jumanji', 'cinderella', 'gravity', 'titanic'];
 
-// all letters for random input
+// all 26 letters in the alphabet for random input
 var alphabetArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
 // shows the number of wins
@@ -13,80 +13,81 @@ var lossCount = 0;
 // drives the win counter
 var rightGuessCounter = 0;
 
-// shows the number of attempts left to input more letters to guess the word
+// shows the number of attempts left to input more letters to guess a one-word movie title
 var guessesRemaining = 10;
 
-// placeholder for the chosen word
-var chosenTitle = "";
+// placeholder for the chosen one-word movie title
+var randomTitle = "";
 
-// placeholders for letters in the word
+// placeholders for letters in the one-word movie title
 var lettersInTitle = [];
 
 // number of blanks in the word 
 var numBlanks = 0;
 
-// placeholder for blanks and successful guesses 
-var blanksAndSuccesses = [];
+// placeholder for blanks and letter with successful hits 
+var blanksAndHits = [];
 
-// placeholder for the wrong guesses  
+// placeholder for letters with no hits 
 var wrongLetters = [];
 
-// Below are functions for the word game
+// Below are functions for the one-word movie title game
 function reset(){
-    chosenTitle = movieTitles[Math.floor(Math.random() * movieTitles.length)];
-    lettersInTitle = chosenTitle.split('');
+    randomTitle = movieTitles[Math.floor(Math.random() * movieTitles.length)];
+    lettersInTitle = randomTitle.split('');     
     numBlanks = lettersInTitle.length;
 
     letterGuessed = 0;
     rightGuessCounter = 0;
     guessesRemaining: 10;
     wrongLetters = [];
-    blanksAndSuccesses = [];
+    blanksAndHits = [];
     alphabetArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
     test=false;
     startGame();
 }
 
 function startGame(){
-    chosenTitle = movieTitles[Math.floor(Math.random() * movieTitles.length)];
-    lettersInTitle = chosenTitle.split('');
+    randomTitle = movieTitles[Math.floor(Math.random() * movieTitles.length)];
+    lettersInTitle = randomTitle.split('');     
     numBlanks = lettersInTitle.length;
 
     rightGuessCounter = 0;
     guessesRemaining = 10;
     wrongLetters = [];
-    blanksAndSuccesses = [];    
+    blanksAndHits = [];    
     alphabetArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
+    // holds the number of underscores that match a one-word movie title
 	for(var i = 0; i< numBlanks; i++){
-        blanksAndSuccesses.push('_');
-        document.querySelector('#titleToGuess').innerText = blanksAndSuccesses;
+        blanksAndHits[i] = "_";    
+        document.querySelector('#titleToGuess').innerText = blanksAndHits;
     }
 
-    document.querySelector('#titleToGuess').innerText = blanksAndSuccesses.join(' ');
+    document.querySelector('#titleToGuess').innerText = blanksAndHits.join(' ');
     document.querySelector('#numGuessesLeft').innerText = guessesRemaining;
     document.querySelector('#winCounter').innerText = winCount;
     document.querySelector('#lossCounter').innerText = lossCount;
     document.querySelector('#wrongGuesses').innerText = wrongLetters;
 
-    console.log(chosenTitle);
+    console.log(randomTitle);
     console.log(lettersInTitle);
     console.log(numBlanks);
-    console.log(blanksAndSuccesses);
+    console.log(blanksAndHits);
 }
 
+// To display correct guessed letters and wrong letters in their corresponding divs and also decreases the count of remaining guesses accordingly   
 function compareLetters (userKey){
-    // console.log('WORKING!');
-    if(chosenTitle.indexOf(userKey) > -1){
+    if(randomTitle.indexOf(userKey) > -1){
         for(var i = 0; i < numBlanks; i++){
             if(lettersInTitle[i] === userKey){
                 rightGuessCounter++;
-                blanksAndSuccesses[i] = userKey;
-                document.querySelector('#titleToGuess').innerText = blanksAndSuccesses.join(' ');
+                blanksAndHits[i] = userKey;
+                document.querySelector('#titleToGuess').innerText = blanksAndHits.join(' ');
             }
         }
 
-        console.log(blanksAndSuccesses);
+        console.log(blanksAndHits);
     }
     else{
         wrongLetters.push(userKey);
@@ -100,12 +101,12 @@ function compareLetters (userKey){
     }
 }
 
+// To display the counts of wins and losses
 function winLose(){
     if(rightGuessCounter === numBlanks){
         winCount++;
 
         document.querySelector('#winCounter').innerText = winCount;
-        //play video
         reset();
     }
     else if(guessesRemaining === 0){
@@ -116,6 +117,7 @@ function winLose(){
     }
 }
 
+// Initiates the code
 startGame();
 
 document.onkeyup = function(event){
@@ -123,10 +125,10 @@ document.onkeyup = function(event){
     var letterGuessed = event.key;
     for(var i = 0; i < alphabetArray.length; i++){
         if(letterGuessed === alphabetArray[i] && test === true){
-            var spliceDword = alphabetArray.splice(i,1);
+            var spliceAlphabetArray = alphabetArray.splice(i,1);
             
             console.log('Double word is = ' + alphabetArray[i]);
-            console.log('Splice word is = ' + spliceDword);
+            console.log('Splice word is = ' + spliceAlphabetArray);
 
             compareLetters(letterGuessed);
             winLose();
@@ -134,9 +136,39 @@ document.onkeyup = function(event){
     }
 }
 
-// Play video corresponding to word/movie title succesfully guessed
-function playVideo(...){
-    var salt = document.getElementById('filmVid');
-    var 
-    if(...)
-}
+
+// Below are my attempts to either play a video(movie trailer) or replace main image with movie poster image when a word is guessed correctly.
+// I haven't figured out how to set the if/else statement that if chosenTitle == 'salt' or another movie title, replace main image
+// with corresponding movie poster or movie trailer.  
+
+// Replace image with video corresponding to word/movie title succesfully guessed
+    // var video = document.createElement("iframe");
+    // elem.setAttribute("src", "https://www.imdb.com/videoembed/vi1236141849");
+    // elem.setAttribute("height", "350");
+    // elem.setAttribute("width", "250");
+    // elem.setAttribute("frameborder", "0");
+    // elem.setAttribute("allow", "autoplay; encrypted-media");
+    // elem.src = "https://www.imdb.com/videoembed/vi1236141849";    
+    //     // if('#titleToGuess' == "salt"){
+    // document.getElementById("img").appendChild(video);         
+    // // }
+
+// Replace starting image with movie poster image corresponding to word/movie title succesfully guessed    
+    // var poster = document.createElement("img");
+    // elem.setAttribute("src", "https://m.media-amazon.com/images/M/MV5BMjIyODA2NDg4NV5BMl5BanBnXkFtZTcwMjg4NDAwMw@@._V1_SY1000_CR0,0,672,1000_AL_.jpg");
+    // elem.setAttribute("alt", "Salt");
+    // elem.src = "https://m.media-amazon.com/images/M/MV5BMjIyODA2NDg4NV5BMl5BanBnXkFtZTcwMjg4NDAwMw@@._V1_SY1000_CR0,0,672,1000_AL_.jpg";    
+    //     // if('#titleToGuess' == "salt"){
+    // document.getElementById("img").appendChild(poster); 
+
+// more attempts...    
+    // function image(thisImg) {
+    //     var img = document.createElement("img");
+    //     img.src = "assets\images\Salt-MoviePoster.jpg";
+    //     document.getElementById('filmDiv').appendChild(img);
+    // }
+    // if (movieTitles.length == 'salt') {
+    //     image("assets\images\Salt-MoviePoster.jpg");
+    // // } else {
+    // //     image("http://fanboygaming.com/wp-content/uploads/2012/10/movie2.jpg");
+    // }
